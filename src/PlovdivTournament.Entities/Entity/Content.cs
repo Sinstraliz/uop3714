2521afb5549c9e25e1e2fc6a36d1166a35664eea
Sinstraliz <blagovest.vp@gmail.com>
2013-09-13 21:55:08 +0300

    Added Tinymce and changed the structure of the  back end solution.

---
+++ src/PlovdivTournament.Entities/Entity/Content.cs
@@ -0,0 +1,34 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+
+namespace PlovdivTournament.Entities.Entity
+{
+    public class Content
+    {
+        public Content() { }
+
+        public Content(string page, string language)
+        {
+            Page = page;
+            Language = language;
+        }
+
+        public virtual Guid Id { get; set; }
+
+        public virtual string Page { get; set; }
+
+        public virtual string Language { get; set; }
+    }
+
+    public class ContentMap : ClassMap<Content>
+    {
+        public ContentMap()
+        {
+            Table("Content");
+            Id(x => x.Id, "Content_Id");
+            Map(x => x.Page, "Page");
+            Map(x => x.Language, "Language");
+        }
+    }
+}
